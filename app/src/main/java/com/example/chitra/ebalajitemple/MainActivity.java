a7233e1d7b30fec431e7f01ce8d82bcaff4bad26
chitra <chitra.atmakuru@gmail.com>
2015-10-29 17:04:48 -0700

    add googlemap

--- app/src/main/java/com/example/chitra/ebalajitemple/MainActivity.java
+++ app/src/main/java/com/example/chitra/ebalajitemple/MainActivity.java
@@ -1,17 +1,37 @@
 package com.example.chitra.ebalajitemple;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton mAroundUs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAroundUs = (ImageButton) findViewById(R.id.Around);
+        final Intent intent = new Intent(this, MapsActivity.class);
+
+        mAroundUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);;
+
+            }
+
+        });
+
     }
+
+    
 
 
 }
